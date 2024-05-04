fa0b6228d186a6a5e4f9f522dadf430774670f46
Polina Rybachuk <polia6v@gmail.com>
2024-05-04 09:10:07 +0200

    zad2

--- test.java
+++ test.java
@@ -1,8 +1,12 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class test {
     public static void main(String[] args) {
+
+        //Pozostałość po pierwszym zadaniu
         try {
             Person person = new Person("Polina Rybachuk", 23);
             System.out.println("Imię: " + person.getName());
@@ -10,5 +14,9 @@ public class test {
         } catch (InvalidAgeException e) {
             System.err.println("Błąd: " + e.getMessage());
         }
+
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Rowerki dziś?");
+
     }
 }
