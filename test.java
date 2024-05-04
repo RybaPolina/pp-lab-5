526b5dcbaba3e7a784fd591a24581a9c12e24c3c
Polina Rybachuk <polia6v@gmail.com>
2024-05-04 10:35:00 +0200

    zad4

--- test.java
+++ test.java
@@ -7,21 +7,36 @@ import company.utils.MathUtils;
 public class test {
     public static void main(String[] args) {
 
-        //Pozostałość po pierwszym zadaniu :)
+        //Zadanie 4 - robię tabelkę
+        Person[] people = new Person[5];
+        int b = 10;
+
         try {
-            Person person = new Person("Polina Rybachuk", 23);
-            System.out.println("Imię: " + person.getName());
-            System.out.println("Wiek: " + person.getAge());
+            people[0] = new Person("Polina Rybachuk", 24);
+            people[1] = new Person("Konrad Ceprynski", 29);
+            people[2] = new Person("Kacper Molewski", 26);
+            people[3] = new Person("Kamila Schetela", 25);
+            people[4] = new Person("Adrianna Jankowska", 30);
+
         } catch (InvalidAgeException e) {
-            System.err.println("Błąd: " + e.getMessage());
+                System.err.println("Błąd: " + e.getMessage());
         }
+        //Licznik do numeru osoby aby dane wyjściowe były bardziej zrozumiałe (zamiast 0 dałam 1 aby indeks był bardziej zrozumiały dla użytkownika)                  
+        int i=1;
+        for (Person osoba : people) {
+            System.out.println("Dane osoby " + i);
+            System.out.println("Imię: " + osoba.getName());
+            System.out.println("Wiek: " + osoba.getAge());
+            
+            int wiek = osoba.getAge();
+            int wartosc = MathUtils.add(wiek, b);
+            System.out.println("Nowa obliczona wartość: " + wartosc);
+    
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("No wyszedł mi wynik sumy " + wartosc);
+            System.out.println();
+            i++;
 
-        //Można było nie definiuować int i wypisać wyświetlanie od razu jako funkcji, ale uznałam że tak będzie bardziej przejrzysty ten kod :)
-        int suma = MathUtils.add(10, 15);
-        System.out.println("Wynik sumy to: " + suma);
-
-        Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Rowerki dziś? Przy okazji wynik sumy to: " + suma);
-
+            }
     }
 }
