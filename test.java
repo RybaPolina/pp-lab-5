8cf71d1aef7d7cc1927dbe29878cd1b43d9c1ef4
Polina Rybachuk <polia6v@gmail.com>
2024-05-04 09:33:04 +0200

    zad3

--- test.java
+++ test.java
@@ -2,11 +2,12 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class test {
     public static void main(String[] args) {
 
-        //Pozostałość po pierwszym zadaniu
+        //Pozostałość po pierwszym zadaniu :)
         try {
             Person person = new Person("Polina Rybachuk", 23);
             System.out.println("Imię: " + person.getName());
@@ -15,8 +16,12 @@ public class test {
             System.err.println("Błąd: " + e.getMessage());
         }
 
+        //Można było nie definiuować int i wypisać wyświetlanie od razu jako funkcji, ale uznałam że tak będzie bardziej przejrzysty ten kod :)
+        int suma = MathUtils.add(10, 15);
+        System.out.println("Wynik sumy to: " + suma);
+
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Rowerki dziś?");
+        messenger.sendMessage("Rowerki dziś? Przy okazji wynik sumy to: " + suma);
 
     }
 }
